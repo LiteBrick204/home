1939105c9da572fd26df0e8d8d01da810fd915dc
LiteBrick204 <None>
2020-12-06 19:43:38 +0530

    Trying to add effects

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var body = document.querySelector('body');
+window.scroll({
+  top: -1000,
+  behavior: 'smooth'
+});
 setInterval(()=>{
 var randomColor = '#'+Math.random().toString(16).slice(-6);
 	body.style.borderColor = randomColor;
-	console.log(randomColor);
 },420)
